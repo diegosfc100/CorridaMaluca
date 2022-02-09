451526f68a937418884948095514da9f0574f163
Diego Rodrigues <diego.sfc100@hotmail.com>
2022-02-09 20:13:56 -0300

    Instanciar classe 'Corredor'

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CorridaMaluca;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Corredor obj = new Corredor();
+        obj.Nome = "diego";
+        Console.WriteLine($"Nome: {obj.Nome}");
+        Console.ReadKey();
+    }
+}
