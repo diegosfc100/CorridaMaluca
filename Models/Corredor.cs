e9573816461e4e21314e9129f0411a734279e98e
Diego Rodrigues <diego.sfc100@hotmail.com>
2022-02-09 20:11:33 -0300

    Adiciona Propriedade 'Nome'

--- Models/Corredor.cs
+++ Models/Corredor.cs
@@ -13,8 +13,17 @@ namespace CorridaMaluca
             this.Nome = nome;
         }
 
-        public string Nome { get; set; } // Propriedade (prop + tab)
-
+        private string nome; // Propriedade (propfull + tab)
+        public string Nome
+        {
+            get { return this.nome; }
+            set { 
+                this.nome = value.ToUpper();
+                if(value.Length > 1) this.nome = value[0].ToString().ToUpper();
+                if(value.Length == 0) this.nome = "d";
+            }
+        }
+        
         public int Correr()
         {
             Random random = new Random();
