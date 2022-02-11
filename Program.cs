031ec81dd893cdec71b88c8c78c1a8875a7f0580
Diego Rodrigues <diego.sfc100@hotmail.com>
2022-02-10 22:29:41 -0300

    Adiciona validação do vencedor

--- Program.cs
+++ Program.cs
@@ -7,17 +7,64 @@ class Program
     {                
         Pista pista1 = new Pista(150, new Corredor("Diego"));
         Pista pista2 = new Pista(150, new Corredor("José"));
+        Pista pista3 = new Pista(150, new Corredor("Maria"));
 
         Boolean ganhou = false;
         
+        Console.WriteLine("$#@ - Corrida Maluca - @#$");
+        Console.WriteLine("Corredores  \n1 - Diego \n2 - José \n3 - Maria ");
+        Console.Write("Em qual corredor vc aposta (1, 2 ou 3):");
+        int aposta = Convert.ToInt32(Console.ReadLine());
+
+        int ganhador = 0;
+
         while(ganhou == false)
         {
             Console.Clear();
-            if (pista1.AtualizarCorrida() == true) ganhou = true;
-            if (pista2.AtualizarCorrida() == true) ganhou = true;
+            if (pista1.AtualizarCorrida() == true) 
+            {
+                ganhou = true;
+                ganhador = 1;
+            }
+            if (pista2.AtualizarCorrida() == true) 
+            {
+                ganhou = true;
+                ganhador = 2;
+            }
+            if (pista3.AtualizarCorrida() == true) 
+            {
+                ganhou = true;
+                ganhador = 3;
+            }
+                                    
             pista1.ExibirPista();            
             pista2.ExibirPista();
+            pista3.ExibirPista();
             Console.ReadKey();         
         }
+        Console.Clear();
+        Console.WriteLine($"O ganhador foi o corredor de número {ganhador}");
+
+        switch (ganhador)
+        {
+            case 1:
+                Console.WriteLine("Diego");
+                break;
+            case 2: 
+                Console.WriteLine("José");
+                break;
+            case 3: 
+                Console.WriteLine("Maria");
+                break;
+        }
+        if(ganhador == aposta)
+        {
+            Console.WriteLine("Parabéns !!!! Vc ganhou");
+        }
+        else
+        {
+            Console.WriteLine("Você perdeu, tente novamente =(");
+        }
+        Console.ReadKey();
     }
 }
